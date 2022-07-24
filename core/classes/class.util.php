4ddb5089518c70df3cbcd15f50e1315b5bdb65a0
Bear <programming@hallhome.us>
2022-07-24 00:05:56 -0500

    trying to fix errors

--- core/classes/class.util.php
+++ core/classes/class.util.php
@@ -1118,7 +1118,8 @@ class Util
     public static function getRemoteFile($url)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, $url -UseBasicParsing);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
