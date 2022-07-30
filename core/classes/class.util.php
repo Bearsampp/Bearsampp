ece92301c058227f70e38ca7e85c1c5a971d05e9
Bear <programming@hallhome.us>
2022-07-30 10:29:41 -0500

    removed -UseBasicParsing till we find a better way.

--- core/classes/class.util.php
+++ core/classes/class.util.php
@@ -1093,7 +1093,7 @@ class Util
     public static function getRemoteFile($url)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url -UseBasicParsing);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
