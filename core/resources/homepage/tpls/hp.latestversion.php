fdc55948e3ebe1c21c20ac6ba861d43a56f04730
Bear <programming@hallhome.us>
2022-07-31 05:55:12 -0500

    close #121

--- core/resources/homepage/tpls/hp.latestversion.php
+++ core/resources/homepage/tpls/hp.latestversion.php
@@ -2,16 +2,4 @@
     <button data-dismiss="alert" class="close" type="button">×</button>
     <h4><?php echo $bearsamppLang->getValue(Lang::CHECK_VERSION_AVAILABLE_TEXT); ?></h4>
     <div class="latestversion-download"></div>
-    <div><?php echo sprintf($bearsamppLang->getValue(Lang::READ_CHANGELOG), '<a href="#" data-toggle="modal" data-target=".modal-changelog">', '</a>'); ?></div>
-</div>
-<div class="modal fade modal-changelog" tabindex="-1" role="dialog" aria-labelledby="modal-changelog-title" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="modal-changelog-title"><?php echo $bearsamppLang->getValue(Lang::CHANGELOG); ?></h4>
-      </div>
-      <div class="modal-body latestversion-changelog"></div>
-    </div>
-  </div>
 </div>
