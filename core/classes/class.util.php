40d16a605c1d900346b645190f4b63934df596ad
Bear <programming@hallhome.us>
2022-07-31 05:49:25 -0500

    Changed method to retrieve contents Close issue #93

--- core/classes/class.util.php
+++ core/classes/class.util.php
@@ -1092,13 +1092,7 @@ class Util
 
     public static function getRemoteFile($url)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        return @curl_exec($ch);
+         return file_get_contents($url);
     }
 
     public static function isPortInUse($port)
