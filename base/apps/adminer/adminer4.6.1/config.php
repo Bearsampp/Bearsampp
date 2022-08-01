a7765d958058f79cdb0ec4c892cd4102b3f20a8f
Bear <programming@hallhome.us>
2022-08-01 01:02:52 -0500

    2022.08.01 First official Full release

---
+++ base/apps/adminer/adminer4.6.1/config.php
@@ -0,0 +1,20 @@
+<?php
+
+global $mysqlPort, $mysqlRootUser, $mysqlRootPwd,
+$mariadbPort, $mariadbRootUser, $mariadbRootPwd,
+$postgresqlPort, $postgresqlRootUser, $postgresqlRootPwd,
+$mongodbPort;
+
+$mysqlPort = 3306;
+$mysqlRootUser = 'root';
+$mysqlRootPwd = '';
+
+$mariadbPort = 3307;
+$mariadbRootUser = 'root';
+$mariadbRootPwd = '';
+
+$postgresqlPort = 5432;
+$postgresqlRootUser = 'postgres';
+$postgresqlRootPwd = '';
+
+$mongodbPort = 27017;
