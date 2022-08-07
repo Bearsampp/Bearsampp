2a1f4b6ddff8427d1297e0225200343ca08d1a82
Bear <programming@hallhome.us>
2022-08-07 06:10:53 -0500

    poor bracing fix.

--- core/classes/actions/class.action.switchPhpExtension.php
+++ core/classes/actions/class.action.switchPhpExtension.php
@@ -9,32 +9,37 @@ class ActionSwitchPhpExtension
     {
         global $bearsamppBins;
 
-        if (isset($args[0]) && !empty($args[0]) && isset($args[1]) && !empty($args[1])) {
-            $onContent = 'extension=' . $args[0];
+        if ( isset( $args[0] ) && !empty( $args[0] ) && isset( $args[1] ) && !empty( $args[1] ) )
+        {
+            $onContent  = 'extension=' . $args[0];
             $offContent = ';extension=' . $args[0];
 
 
-            $phpiniContent = file_get_contents($bearsamppBins->getPhp()->getConf());
-            if ($args[1] == self::SWITCH_ON) {
-                $phpiniContent = str_replace($offContent, $onContent, $phpiniContent);
-            } elseif ($args[1] == self::SWITCH_OFF) {
-                $phpiniContent = str_replace($onContent, $offContent, $phpiniContent);
+            $phpiniContent = file_get_contents( $bearsamppBins->getPhp()->getConf() );
+            if ( $args[1] == self::SWITCH_ON )
+            {
+                $phpiniContent = str_replace( $offContent, $onContent, $phpiniContent );
+            }
+            elseif ( $args[1] == self::SWITCH_OFF )
+            {
+                $phpiniContent = str_replace( $onContent, $offContent, $phpiniContent );
             }
 
-            $phpiniContentOr = file_get_contents($bearsamppBins->getPhp()->getConf());
-            if ($phpiniContent == $phpiniContentOr && file_exists($bearsamppBins->getPhp()->getSymlinkPath() . '/ext/php_' . $args[0] . '.dll')) {
-                $extsIni = $bearsamppBins->getPhp()->getExtensionsFromConf();
-                $latestExt = (end($extsIni) == '0' ? ';' : '');
-                $latestExt .= 'extension=' . key($extsIni);
-               }
+            $phpiniContentOr = file_get_contents( $bearsamppBins->getPhp()->getConf() );
+            if ( $phpiniContent == $phpiniContentOr && file_exists( $bearsamppBins->getPhp()->getSymlinkPath() . '/ext/php_' . $args[0] . '.dll' ) )
+            {
+                $extsIni   = $bearsamppBins->getPhp()->getExtensionsFromConf();
+                $latestExt = (end( $extsIni ) == '0' ? ';' : '');
+                $latestExt .= 'extension=' . key( $extsIni );
+
                 $phpiniContent = str_replace(
                     $latestExt,
                     $latestExt . PHP_EOL . $onContent,
                     $phpiniContent
                 );
+            }
 
-
-            file_put_contents($bearsamppBins->getPhp()->getConf(), $phpiniContent);
+            file_put_contents( $bearsamppBins->getPhp()->getConf(), $phpiniContent );
         }
     }
 }
