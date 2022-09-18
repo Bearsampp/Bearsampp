b2de3429d26671f6c4b25a6112980526cf28df29
Jacob Waisner <jwaisner@users.noreply.github.com>
2022-09-18 01:14:49 -0500

    styling

--- core/resources/homepage/tpls/hp.index.apache.php
+++ core/resources/homepage/tpls/hp.index.apache.php
@@ -31,49 +31,49 @@
     </div>
 </div>
 <div class = "border grid-list mt-3">
-    <div class = "row-fluid mt-3">
+    <div class = "row-fluid mt-2">
         <div class = "col-lg-12 section-top">
             <h3><i class = "fa fa-gear"></i> <?php echo $bearsamppLang->getValue( Lang::MODULES ); ?> <small></small></h3>
         </div>
     </div>
     <div class = "row-fluid">
-        <div class = "col-lg-12 apache-moduleslist d-flex flex-wrap">
+        <div class = "col-lg-12 apache-moduleslist d-flex flex-wrap mb-2">
             <span class = "loader"><img src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/loader.gif'; ?>" /></span>
         </div>
     </div>
 </div>
 <div class = "border grid-list mt-3">
-    <div class = "row-fluid mt-3">
+    <div class = "row-fluid mt-2">
         <div class = "col-lg-12 section-top">
             <h3><i class = "fa fa-link"></i> <?php echo $bearsamppLang->getValue( Lang::ALIASES ); ?> <small></small></h3>
         </div>
     </div>
     <div class = "row-fluid">
-        <div class = "col-lg-12 apache-aliaseslist d-flex flex-wrap">
+        <div class = "col-lg-12 apache-aliaseslist d-flex flex-wrap mb-2">
             <span class = "loader"><img src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/loader.gif'; ?>" /></span>
         </div>
     </div>
 </div>
 <div class = "border grid-list mt-3">
-    <div class = "row-fluid mt-3">
+    <div class = "row-fluid mt-2">
         <div class = "col-lg-12 section-top">
             <h3><i class = "fa fa-folder"></i> <?php echo $bearsamppLang->getValue( Lang::MENU_WWW_DIRECTORY ); ?> <small></small></h3>
         </div>
     </div>
     <div class = "row-fluid">
-        <div class = "col-lg-12 apache-wwwdirectory d-flex flex-wrap">
+        <div class = "col-lg-12 apache-wwwdirectory d-flex flex-wrap mb-2">
             <span class = "loader"><img src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/loader.gif'; ?>" /></span>
         </div>
     </div>
 </div>
 <div class = "border grid-list mt-3">
-    <div class = "row-fluid mt-3">
+    <div class = "row-fluid mt-2">
         <div class = "col-lg-12 section-top">
             <h3><i class = "fa fa-globe"></i> <?php echo $bearsamppLang->getValue( Lang::VIRTUAL_HOSTS ); ?> <small></small></h3>
         </div>
     </div>
     <div class = "row-fluid">
-        <div class = "col-lg-12 apache-vhostslist d-flex flex-wrap">
+        <div class = "col-lg-12 apache-vhostslist d-flex flex-wrap mb-2">
             <span class = "loader"><img src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/loader.gif'; ?>" /></span>
         </div>
     </div>
