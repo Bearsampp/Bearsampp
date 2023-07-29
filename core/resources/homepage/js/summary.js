e9b587c079001a15485a3e59e39419292a2baf8d
Bear <programming@hallhome.us>
2023-07-29 15:53:59 -0500

    Rewrite fixing issue #318

--- core/resources/homepage/js/summary.js
+++ core/resources/homepage/js/summary.js
@@ -5,31 +5,31 @@ $(document).ready(function() {
         proc: 'summary'
       },
       success: function(data) {
-        $('.summary-binapache').prepend(data.binapache);
+        $('.summary-binapache').append(data.binapache);
         $('.summary-binapache').find('.loader').remove();
 
-        $('.summary-binfilezilla').prepend(data.binfilezilla);
+        $('.summary-binfilezilla').append(data.binfilezilla);
         $('.summary-binfilezilla').find('.loader').remove();
 
-        $('.summary-binmailhog').prepend(data.binmailhog);
+        $('.summary-binmailhog').append(data.binmailhog);
         $('.summary-binmailhog').find('.loader').remove();
 
-        $('.summary-binmariadb').prepend(data.binmariadb);
+        $('.summary-binmariadb').append(data.binmariadb);
         $('.summary-binmariadb').find('.loader').remove();
 
-        $('.summary-binmysql').prepend(data.binmysql);
+        $('.summary-binmysql').append(data.binmysql);
         $('.summary-binmysql').find('.loader').remove();
 
-        $('.summary-binpostgresql').prepend(data.binpostgresql);
+        $('.summary-binpostgresql').append(data.binpostgresql);
         $('.summary-binpostgresql').find('.loader').remove();
 
-        $('.summary-binmemcached').prepend(data.binmemcached);
+        $('.summary-binmemcached').append(data.binmemcached);
         $('.summary-binmemcached').find('.loader').remove();
 
-        $('.summary-binnodejs').prepend(data.binnodejs);
+        $('.summary-binnodejs').append(data.binnodejs);
         $('.summary-binnodejs').find('.loader').remove();
 
-        $('.summary-binphp').prepend(data.binphp);
+        $('.summary-binphp').append(data.binphp);
         $('.summary-binphp').find('.loader').remove();
       }
     });
