a5ae194b08d3b3a2087f5ed7b7015fbc3e21ab33
Bear <programming@hallhome.us>
2024-05-15 05:44:12 -0500

    updated to reflect codium supporting us now.  We can remove them if they change their mind. Removed neard

--- core/resources/homepage/homepage.php
+++ core/resources/homepage/homepage.php
@@ -6,7 +6,7 @@
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <meta name = "description" content = "">
     <meta name = "author" content = "">
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/jquery/jquery-3.6.1.min.js"></script>
+    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/jquery/jquery-3.7.1.min.js"></script>
     <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/jquery/jquery-migrate-3.4.0.min.js"></script>
     <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/bootstrap/popper.min.js"></script>
     <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/bootstrap/bootstrap.min.js"></script>
