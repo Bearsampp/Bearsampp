b391996f642edd3454185239188eda2977c885d0
N6REJ <programming@hallhome.us>
2024-06-12 16:21:17 +0000

    🔄 synced local 'core/' with remote 'core/'

--- core/resources/homepage/js/_commons.js
+++ core/resources/homepage/js/_commons.js
@@ -1,8 +1,27 @@
-/*$.ajaxSetup({
-  type: "POST",
-  cache: false,
-  dataType: "json",
-  url: "1fd5bfc5c72323f1d019208088a6de21/ajax.php"
-});*/
+/*
+ * Copyright (c) 2021-2024 Bearsampp
+ * License:  GNU General Public License version 3 or later; see LICENSE.txt
+ * Author: Bear
+ * Website: https://bearsampp.com
+ * Github: https://github.com/Bearsampp
+ */
 
+/**
+ * @fileOverview This script sets up the AJAX configuration and defines the URL for AJAX requests.
+ */
+
+/**
+ * @constant {string} AJAX_URL - The URL endpoint for AJAX requests.
+ */
 const AJAX_URL = "1fd5bfc5c72323f1d019208088a6de21/ajax.php"
+
+/**
+ * @deprecated The following AJAX setup is commented out and should not be used.
+ *
+ * $.ajaxSetup({
+ *   type: "POST",
+ *   cache: false,
+ *   dataType: "json",
+ *   url: "1fd5bfc5c72323f1d019208088a6de21/ajax.php"
+ * });
+ */
