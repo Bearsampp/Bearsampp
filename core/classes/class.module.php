9e2911b3c619287d2a461e6bbf0f3865e5240248
N6REJ <programming@hallhome.us>
2024-07-21 04:45:15 +0000

    🔄 synced local 'core/' with remote 'core/'

--- core/classes/class.module.php
+++ core/classes/class.module.php
@@ -72,13 +72,6 @@ abstract class Module
         if ($bearsamppRoot->isRoot()) {
             $this->createSymlink();
         }
-
-        // TODO Can we replace this or remove it since we dont use bundles?
-        if ($this->bearsamppConfRaw !== false) {
-            if (isset($this->bearsamppConfRaw[self::BUNDLE_RELEASE])) {
-                $this->release = $this->bearsamppConfRaw[self::BUNDLE_RELEASE];
-            }
-        }
     }
 
     /**
